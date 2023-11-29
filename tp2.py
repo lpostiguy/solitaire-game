14d4ec4b419789592b43022003eea1f08c63d1b0
TNoahT <noahttaillon@gmail.com>
2023-11-29 09:40:32 -0500

    fonction brasser et tests unitaires

--- tp2.py
+++ tp2.py
@@ -2,6 +2,43 @@
 # tel qu'indiqué dans la description du TP2.  Le code ici correspond
 # à l'exemple donné dans la description.
 
+import random
+import math
+
+
+# La fonction 'brasser' prend en paramètre un tableau (tab) ne contenant que 
+# des éléments allant de 0 à 51 en ordre croissant. Ce tableau sera 
+# mélangé selon le principe suivant : échangeons le dernier élément n du
+# tableau et échangeons le avec un élément aléatoire le précédant. Puis,
+# même chose avec n-1, jusqu'à ce que tous les éléments aient été échangés.
+# La fonction retourne ce tableau mélangé
+def brasser(tab):
+    for i in range(len(tab)):
+
+        # Variable permettant de cycler au travers des éléments du tableau, en
+        # partant par la fin.
+        indice_dernier_element = len(tab)-1-i
+        
+        # Choix aléatoire précédant le dernier élément
+        aleatoire = math.floor(random.random()*len(tab)-1-i)
+
+        temp = tab[indice_dernier_element]
+        tab[indice_dernier_element] = tab[aleatoire]
+        tab[aleatoire] = temp
+    return tab
+
+
+# Difficile de faire un test unitaire
+def testBrasser():
+    tab = []
+    for i in range(52):
+        tab.append(i)
+        
+    tab_brasse = tab.copy()
+    assert brasser(tab_brasse) != tab
+    assert brasser([0]) == [0]
+    assert brasser([]) == []
+
 def init():
 
     # changer le contenu HTML de l'élément racine
